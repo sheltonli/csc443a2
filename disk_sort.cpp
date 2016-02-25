95eadcc5c0d7aa7dd58767a96a901c8014c50cfb
sheltonli <shelton.li.ts@gmail.com>
2016-02-25 16:48:21 -0500

    Added changes from disk_sort2 except realloc to disk_sort

--- disk_sort.cpp
+++ disk_sort.cpp
@@ -10,6 +10,7 @@ int chunk_size;
 int records_per_chunk;
 int last_chunk;
 int records_in_last_chunk;
+int memory_size;
 
 typedef struct record {
   int uid1;
@@ -18,7 +19,7 @@ typedef struct record {
 
 void read_into_buffer(FILE * fp, Record * buffer);
 void phase1 (FILE* fp);
-
+void print_buffer_content (Record * buffer);
 
 /**
 * Compares two records a and b 
@@ -56,8 +57,9 @@ int main(int argc, char const *argv[])
   num_runs = atoi(argv[4]);
 
 
-  /* All sizes in blocks */
-	file_size = fsize/block_size;
+  /* All sizes are in blocks */
+  file_size = fsize/block_size;
+  memory_size = mem_size/block_size;
 
   chunk_size = file_size/num_runs;
   records_per_chunk = chunk_size*block_size/sizeof(Record);
@@ -66,11 +68,11 @@ int main(int argc, char const *argv[])
   records_in_last_chunk = last_chunk*block_size/sizeof(Record);
 
 
-  if ((chunk_size > mem_size) || ((chunk_size == mem_size) && (last_chunk > mem_size % block_size))){
-  	printf("Not enough runs \n");
-  	return 1;
+  /* chunk size should be 1 block smaller than main memory size */
+  if ((records_per_chunk > mem_size) || ((records_per_chunk == mem_size) && (records_in_last_chunk > mem_size % block_size))){
+    printf("Not enough runs \n");
+    return 1;
   }
-
 
   phase1(fp);
 
@@ -81,39 +83,57 @@ int main(int argc, char const *argv[])
 }
 
 
-//pmms(FILE* fp)
-
-
+//r edges.dat 209715200 8192 10
 void phase1 (FILE* fp){
-	  FILE * fp_sorted;
-  	if (!(fp_sorted = fopen ("sorted.dat", "wb" )))
-    	exit(EXIT_FAILURE);
+    FILE * fp_sorted;
+    if (!(fp_sorted = fopen ("sorted.dat", "wb" )))
+      exit(EXIT_FAILURE);
 
- 	  /* Allocate buffer for reading the file */
- 		Record * buffer = (Record *) calloc (records_per_chunk, sizeof(Record));
-
+    /* Allocate buffer for reading the file */
+    Record * buffer = (Record *) calloc (records_per_chunk, sizeof(Record));
+    
     /* Read file in chunks to the buffer */
     int result = fread (buffer, sizeof(Record), records_per_chunk, fp);
 
-		while(result == records_per_chunk){
-			/* Sort in main memory */
-			qsort (buffer, records_per_chunk, sizeof(Record), compare);
-			/* Write sorted buffer to a new file */
-			fwrite(buffer, sizeof(Record), records_per_chunk, fp_sorted);
-			result = fread (buffer, sizeof(Record), records_per_chunk, fp);
-		}
-		/* Read last chunk */
-		if (last_chunk != 0){
-			int buf_size = last_chunk*block_size*sizeof(Record);
-			void * small_buffer = realloc(buffer, buf_size);
-			/* Sort in main memory */
-			qsort (small_buffer, records_per_chunk, sizeof(Record), compare);
-			/* Write sorted buffer to a new file */
-			fwrite(buffer, sizeof(Record), records_in_last_chunk, fp_sorted);
-		}
+    while(result == records_per_chunk){
+      /* Sort in main memory */
+      qsort (buffer, records_per_chunk, sizeof(Record), compare);
+      /* Write sorted buffer to a new file */
+      fwrite(buffer, sizeof(Record), records_per_chunk, fp_sorted);
+      //print_buffer_content (buffer);
+      result = fread (buffer, sizeof(Record), records_per_chunk, fp);
+    }
+    // free(buffer);
+    /* Read last chunk */
+    if (last_chunk != 0){
+      int buf_size = last_chunk*block_size*sizeof(Record);
+      Record * small_buffer = (Record *) realloc(buffer, buf_size);
+      /* Sort in main memory */
+      qsort (small_buffer, records_in_last_chunk, sizeof(Record), compare);
+      /* Write sorted buffer to a new file */
+      fwrite(small_buffer, sizeof(Record), records_in_last_chunk, fp_sorted);
+      free(small_buffer);
+    } else {
+      free(buffer);
+    }
 }
 
 
+void phase2(FILE* fp){
+  /* total input buffers = total number of blocks 
+  tha can fit in memeory minus 1 block for output buffer */
+  int total_input_buffers = num_runs-1;
+  /* total number of blocks per buffer */
+  int blocks_per_buffer = mem_size/num_runs;
+  
+  /* read one block from each chunk and put it into buffers*/
+}
 
+
+void print_buffer_content (Record * buffer){
+  for (int i = 0; i < records_per_chunk; i++){
+    printf("uid2: %d, ", (buffer + i)->uid2);
+  }
+}
 
 
