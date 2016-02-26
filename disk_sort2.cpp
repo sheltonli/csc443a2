192fdc5784ba04d6402805d04aa70cf9b2f46cf1
sheltonli <shelton.li.ts@gmail.com>
2016-02-25 22:41:34 -0500

    testing, trying to get k lists

--- disk_sort2.cpp
+++ disk_sort2.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "Merge.h"
+// #include "InputBuffer.h"
 
 int mem_size;
 int block_size;
@@ -8,17 +10,14 @@ int num_runs;
 int file_size;
 int chunk_size;
 int records_per_chunk;
-int last_chunk;
+int excess_blocks;
 int records_in_last_chunk;
-
-typedef struct record {
-  int uid1;
-  int uid2;
-} Record;
+int memory_size;
+int last_chunk;
 
 void read_into_buffer(FILE * fp, Record * buffer);
 void phase1 (FILE* fp);
-
+void print_buffer_content (Record * buffer);
 
 /**
 * Compares two records a and b 
@@ -56,27 +55,30 @@ int main(int argc, char const *argv[])
   num_runs = atoi(argv[4]);
 
 
-  /* All sizes in blocks */
-	file_size = fsize/block_size;
+  /* All sizes are in blocks */
+  file_size = fsize/block_size;
+  memory_size = mem_size/block_size;
 
   chunk_size = file_size/num_runs;
   records_per_chunk = chunk_size*block_size/sizeof(Record);
 
-  last_chunk = file_size % num_runs;
-  records_in_last_chunk = last_chunk*block_size/sizeof(Record);
+  excess_blocks = file_size % num_runs;
+
+  if (excess_blocks != 0){
+    chunk_size = chunk_size + excess_blocks;
+    records_per_chunk = chunk_size*block_size/sizeof(Record);
+
+    last_chunk = file_size - (num_runs - 1) * chunk_size;
+    records_in_last_chunk = last_chunk*block_size/sizeof(Record);
+  }
 
 
-  /* COMMENT FOR VALERY 
-    Don't we want to check records_per_chunk against mem_size?
-    We are going to allocate memory of size records_per_chunk.
-    That's the true size of the chunk and not chunk_size?
-    Same applies for the 2nd part of the OR where we need to check records_in_last_chunk.
-    
-    THIS NEEDS TO BE FIXED.  FORGOT
-  */
+
+
+  /* chunk size should be 1 block smaller than main memory size */
   if ((records_per_chunk > mem_size) || ((records_per_chunk == mem_size) && (records_in_last_chunk > mem_size % block_size))){
-  	printf("Not enough runs \n");
-  	return 1;
+    printf("Not enough runs \n");
+    return 1;
   }
 
   phase1(fp);
@@ -88,66 +90,72 @@ int main(int argc, char const *argv[])
 }
 
 
-//pmms(FILE* fp)
-
-
+//r edges.dat 209715200 8192 10
 void phase1 (FILE* fp){
-	  FILE * fp_sorted;
-  	if (!(fp_sorted = fopen ("sorted.dat", "wb" )))
-    	exit(EXIT_FAILURE);
-
- 	  /* Allocate buffer for reading the file */
- 		Record * buffer = (Record *) calloc (records_per_chunk, sizeof(Record));
-
+    /* Allocate buffer for reading the file */
+    Record * buffer = (Record *) calloc (records_per_chunk, sizeof(Record));
+    
     /* Read file in chunks to the buffer */
     int result = fread (buffer, sizeof(Record), records_per_chunk, fp);
 
-		while(result == records_per_chunk){
-			/* Sort in main memory */
-			qsort (buffer, records_per_chunk, sizeof(Record), compare);
-			/* Write sorted buffer to a new file */
-			fwrite(buffer, sizeof(Record), records_per_chunk, fp_sorted);
-			result = fread (buffer, sizeof(Record), records_per_chunk, fp);
-		}
-		/* Read last chunk */
-		if (last_chunk != 0){
-			
-      /* NOTE FOR VALERY 
-        Before it was doing another calculation of last_chunk*block_size*sizeof(Record).
-        Not sure why it was multiplying by sizeof(Record).
-        I'm assuming you wanted to do a calculation like the ones above (for records_per_chunk and records_in_last_chunk).
-        If that's the case, then we can just use records_in_last_chunk?
+
+    int count = 1;
+    char file_name[20];
+    while(result == records_per_chunk){
+      sprintf(file_name, "output%d.dat", count);
+      FILE * fp_sorted;
+      if (!(fp_sorted = fopen (file_name, "wb" ))){
+        exit(EXIT_FAILURE);
+      }
         
-        IM MULTIPLYING BU SIZEOF(RECORD) BECAUSE REALLOC TAKES NUMBER OF BYTES AND CALLOC NUMBER OF ELEMENTS
-      */
+      /* Sort in main memory */
+      qsort (buffer, records_per_chunk, sizeof(Record), compare);
+      /* Write sorted buffer to a new file */
+      fwrite(buffer, sizeof(Record), records_per_chunk, fp_sorted);
+      //print_buffer_content (buffer);
+      result = fread (buffer, sizeof(Record), records_per_chunk, fp);
+      count ++;
+      fclose(fp_sorted);
+    }
 
-			Record * small_buffer = (Record *) realloc(buffer, records_in_last_chunk);
-
-      /* NOTE FOR VALERY 
-        Don't we want to sort the remaining records which is of size records_in_last_chunk?
-      
-      	TRUE. MY BAD
-      */
-			qsort (small_buffer, records_in_last_chunk, sizeof(Record), compare);
-			
-      /* NOTE FOR VALERY 
-        Aren't we writing what's in small_buffer?
-      */
-			fwrite(small_buffer, sizeof(Record), records_in_last_chunk, fp_sorted);
-
-      /* NOTE FOR VALERY
-        I think we should free the memory. This way is okay right?
-        CERTAINLY
-      */
+    /* Read last chunk */
+    if (excess_blocks != 0){
+      sprintf(file_name, "output%d.dat", count);
+      FILE * fp_sorted;
+      if (!(fp_sorted = fopen (file_name, "wb" ))){
+        exit(EXIT_FAILURE);
+      }
+      int buf_size = last_chunk*block_size*sizeof(Record);
+      Record * small_buffer = (Record *) realloc(buffer, buf_size);
+      /* Sort in main memory */
+      qsort (small_buffer, records_in_last_chunk, sizeof(Record), compare);
+      /* Write sorted buffer to a new file */
+      fwrite(small_buffer, sizeof(Record), records_in_last_chunk, fp_sorted);
+      fclose(fp_sorted);
       free(small_buffer);
-		}
-    else {
+    } else {
       free(buffer);
     }
-   
 }
 
 
+void phase2(FILE* fp){
+  /*  */
+  int total_input_buffers = num_runs + 1;
+  /* total number of blocks per buffer */
+  int records_per_buffer = (mem_size/total_input_buffers) / block_size;
 
+  // for (int i = 0; i < num_runs; i++){
+
+  // }  
+  /* read one block from each chunk and put it into buffers*/
+}
+
+
+void print_buffer_content (Record * buffer){
+  for (int i = 0; i < records_per_chunk; i++){
+    printf("uid2: %d, ", (buffer + i)->uid2);
+  }
+}
 
 
